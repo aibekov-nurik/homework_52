c713bd8232991f1c26bd51a4bd52ee0129674370
aibekov nurkerim <aibekovnurkerim@gmai.com>
2024-06-27 05:22:28 +0600

    Создал шаблон для отображниедеталей задач

--- todo_project/tasks/models.py
+++ todo_project/tasks/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Task(models.Model):
+    STATUS_CHOICES = [
+        ('new', 'Новая'),
+        ('in_progress', 'В процессе'),
+        ('done', 'Сделано'),
+    ]
+
+    description = models.TextField(verbose_name='Описание')
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='new', verbose_name='Статус')
+    due_date = models.DateField(null=True, blank=True, verbose_name='Дата выполнения')
+
+    def __str__(self):
+        return self.description
