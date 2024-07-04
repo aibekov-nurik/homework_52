ae748ab808e8b757d00f984141a303010f1dea67
aibekov nurkerim <aibekovnurkerim@gmai.com>
2024-07-04 14:19:03 +0600

    Изменени

--- todo_project/tasks/forms.py
+++ todo_project/tasks/forms.py
@@ -1,8 +1,7 @@
-# tasks/forms.py
 from django import forms
 from .models import Task
 
 class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
-        fields = ['description', 'status', 'due_date']
+        fields = ['description', 'description_detail', 'status', 'due_date']
